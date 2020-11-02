d078791514f822c62bce202872145ba7ca09b5ab
Srivastava <akashsrivastava@deloitte.com>
2020-11-01 22:30:30 -0500

    Added css

--- src/containers/Header.container.react.tsx
+++ src/containers/Header.container.react.tsx
@@ -9,7 +9,6 @@ interface Props {
 const Header: React.FC<Props> = ({searchString, setSearchString}) => {
     return (
         <div className={styles.headerContainer}>
-            Fixed Header
             <input type="text" placeholder="Search.." value= {searchString} onChange={(e) =>setSearchString(e.target.value)}/>
         </div>
     )
