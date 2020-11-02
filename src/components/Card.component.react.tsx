bae3f0224bf2fb9359938c37570f2c65594a74ef
Srivastava <akashsrivastava@deloitte.com>
2020-11-01 19:16:16 -0500

    Fixed cards

--- src/components/Card.component.react.tsx
+++ src/components/Card.component.react.tsx
@@ -1,15 +1,40 @@
+import {CardType, QueryParametersType} from "./../utilities/Cards.type";
+
+import {CardParametersLabelMappingEnum} from "./../utilities/Cards.constant";
 import React from 'react';
 import styles from './../styles/Cards.module.css';
 
 interface Props {
-    data: any
+    data: CardType
 }
 
 const Card: React.FC<Props> = ({data}) => {
+    const keys = []
     return (
         <figure className={styles.figureDetails}>
-            <img src={data.imageUrl} className={styles.imageContainer}/>
-            <figcaption>{data.name}</figcaption>
+            {
+            (data.imageUrl != null)?
+                <img src={data.imageUrl} alt="No Image Available" className={styles.imageContainer}/>:
+                <img alt="No Image Available" className={styles.imageContainer}/>
+            }
+            <figcaption>
+                <div>
+                    <b>Name: </b> 
+                    {data.name}
+                </div>
+                <div>
+                    <b>Text: </b> 
+                    {data.text}
+                </div>
+                <div>
+                    <b>Set Name: </b> 
+                    {data.setName}
+                </div>
+                <div>
+                    <b>Type: </b> 
+                    {data.type}
+                </div>
+            </figcaption>
         </figure>
     )
 }
--- src/utilities/Cards.constant.ts
+++ src/utilities/Cards.constant.ts
@@ -1,2 +1,9 @@
 export const MAX_RESULT_LIMIT = 20;  //Number of API results
 export const MAX_HEIGHT_BEFORE_NEXT_API_CALL = .90; //Send API request 
+
+export enum CardParametersLabelMappingEnum {
+    name = "Name",
+    text = "Text",
+    setName = "Set Name",
+    type = "Type"
+}
